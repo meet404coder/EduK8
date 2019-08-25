42e05c8dad41bbc67640c0fb8cfd0b066eb7e9c3
Kapil Verma <kapilv1999@gmail.com>
2019-08-25 11:12:45 +0530

    Modified Option to view complete post

--- app/src/main/java/com/vision/eduk8/ViewPostActivity.java
+++ app/src/main/java/com/vision/eduk8/ViewPostActivity.java
@@ -5,6 +5,8 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.sun.mail.iap.ByteArray;
 
@@ -25,5 +27,17 @@ public class ViewPostActivity extends AppCompatActivity {
         tags = b.getString("tags");
         byteArray = b.getByteArray("bitmap");
         Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+
+        TextView tv1 = findViewById(R.id.tvTitle);
+        TextView tv2 = findViewById(R.id.tvBody);
+        TextView tv3 = findViewById(R.id.tvTags);
+        TextView tv4 = findViewById(R.id.tvAuthor);
+        ImageView iv = findViewById(R.id.ivView);
+
+        tv1.setText(title);
+        tv2.setText(body);
+        tv3.setText(tags);
+        tv4.setText(author);
+        iv.setImageBitmap(bitmap);
     }
 }
